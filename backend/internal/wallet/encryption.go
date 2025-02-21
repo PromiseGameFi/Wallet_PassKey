827f9a9d2d8ea51e2259c5bd70e055817f89d77e
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-02-21 10:34:51 +0100

    Update encryption.go

--- backend/internal/wallet/encryption.go
+++ backend/internal/wallet/encryption.go
@@ -1,7 +1,28 @@
 package wallet
 
-func EncryptSeed(seed []byte, key []byte) ([]byte, error) {
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
 
+func EncryptSeed(seed []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(seed))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], seed)
+
+	return ciphertext, nil
 }
 
 func DecryptSeed(ciphertext []byte, key []byte) ([]byte, error) {
