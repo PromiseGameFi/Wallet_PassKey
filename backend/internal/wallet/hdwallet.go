48ea8a6b5d563d28cf8b555ea5cffe963a05b649
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-02-21 10:36:33 +0100

    Update hdwallet.go

--- backend/internal/wallet/hdwallet.go
+++ backend/internal/wallet/hdwallet.go
@@ -1,6 +1,8 @@
 package wallet
 
 import (
+	"crypto/rand"
+
 	"github.com/ethereum/go-ethereum/accounts"
 )
 
@@ -15,12 +17,30 @@ func NewHDWallet() (*HDWallet, error) {
 		return nil, err
 	}
 
+	masterKey, err := deriveMasterKey(seed)
+	if err != nil {
+		return nil, err
+	}
+
+	return &HDWallet{
+		MasterSeed: seed,
+		MasterKey:  masterKey,
+	}, nil
 }
 
 func generateMasterSeed() ([]byte, error) {
-
+	seed := make([]byte, 64)
+	_, err := rand.Read(seed)
+	if err != nil {
+		return nil, err
+	}
+	return seed, nil
 }
 
 func deriveMasterKey(seed []byte) (*accounts.Account, error) {
-
+	masterKey, err := hd.NewMaster(seed, []byte("m/44'/60'/0'/0"))
+	if err != nil {
+		return nil, err
+	}
+	return masterKey, nil
 }
