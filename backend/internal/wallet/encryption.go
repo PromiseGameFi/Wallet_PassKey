f70aa38a8c1b351bc7ffac8473e9593a0180e0cd
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-02-21 10:34:38 +0100

    Update encryption.go

--- backend/internal/wallet/encryption.go
+++ backend/internal/wallet/encryption.go
@@ -1,22 +1,6 @@
 package wallet
 
-import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"io"
-)
-
 func EncryptSeed(seed []byte, key []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	ciphertext := make([]byte, aes.BlockSize+len(seed))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
 
 }
 
