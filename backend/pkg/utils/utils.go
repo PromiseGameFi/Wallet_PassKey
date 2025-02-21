e014eb9aff8514f02cb2987fc5caaea1e1dcc927
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-02-21 10:34:03 +0100

    Update utils.go

--- backend/pkg/utils/utils.go
+++ backend/pkg/utils/utils.go
@@ -1,15 +1,1 @@
 package utils
-
-import (
-	"crypto/rand"
-	"encoding/hex"
-)
-
-func GenerateRandomKey() (string, error) {
-	key := make([]byte, 32)
-	_, err := rand.Read(key)
-	if err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(key), nil
-}
